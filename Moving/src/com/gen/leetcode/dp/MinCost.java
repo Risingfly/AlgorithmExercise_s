bdb0409230ecfccfe719057ac1f015f22780bede
genge <243358411@qq.com>
2019-05-22 15:33:49 +0800

    dp

--- Moving/src/com/gen/leetcode/dp/MinCost.java
+++ Moving/src/com/gen/leetcode/dp/MinCost.java
@@ -14,36 +14,29 @@ public class MinCost {
         if (n < 0 || m < 0){
             return 0;
         }
-//        dp[i][j]表示A编辑成B的最小代价，其中i= 0 | j = 0表示空串相关的操作
-//        因此需要多开辟一个空间来计算空串相关的操作，故 n + 1,m + 1
         int[][] dp = new int[n + 1][m + 1];
-//
-//        A[0...i]编辑成B[]空串的代价，即删除A中所有字符
-        for (int i = 0; i <= n ; i++) {
-            dp[i][0] = c1 * i;
+        dp[0][0] = 0;
+//        第0列
+        for (int i = 1; i < n + 1; i++) {
+            dp[i][0] = i * c1;
         }
-//        A[]编辑成B[0...i]的代价，即A插入对应的字符
-        for (int i = 0; i <= m; i++) {
-            dp[0][i] = c0 * i;
+//        第0行
+        for (int i = 1; i < m + 1; i++) {
+            dp[0][i] = i * c0;
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (A.charAt(i - 1) == B.charAt(i - 1)){
-//                    若对应的A和B最后（当前）一个字符相等，
-//                    只需从i-1 和 j - 1调整即可，代价和之前的相等
-//                    都是从插入 删除 替换这三种代价中选出最小的代价为dp[i][j]
-                    dp[i][j] = Math.min(dp[i - 1][j - 1],Math.min(dp[i - 1][j] +c1,
-                            dp[i][j - 1] + c0));
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+//                i -1 == j - 1代表当前的字符串最后一个字符是否相等而非i == j
+//                字符是从0开始,因此需要规划到n m
+                if (A.charAt(i - 1) == B.charAt(j - 1)){
+                    dp[i][j] = Math.min(dp[i - 1][j - 1],Math.min((dp[i][j - 1] + c0),(dp[i - 1][j] + c1)));
                 }else {
-//                    若对应的A和B最后（当前）一个字符不相等，
-//                    则需将之前的调整再加上最后一个字符的代价
-//                    都是从插入 删除 替换这三种代价中选出最小的代价为dp[i][j]
-                    dp[i][j] = Math.min(dp[i - 1][j - 1] + c2,Math.min(dp[i - 1][j] +c1,
-                            dp[i][j - 1] + c0));
+                    dp[i][j] = Math.min((dp[i - 1][j - 1] + c2),Math.min((dp[i][j - 1] + c0),(dp[i - 1][j] + c1)));
                 }
             }
         }
         return dp[n][m];
     }
+
 }
